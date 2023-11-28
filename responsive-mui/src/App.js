66445d76b0ce93a16b30184f9310c923cba4cd8d
Alexander Farber <farber72@outlook.de>
2023-11-28 21:28:46 +0100

    Add search field and icon to the navbar

--- responsive-mui/src/App.js
+++ responsive-mui/src/App.js
@@ -2,11 +2,13 @@ import Feed from "./components/Feed";
 import Sidebar from "./components/Sidebar";
 import Rightbar from "./components/Rightbar";
 import { Box, Stack } from "@mui/material";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Box>
-      <Stack direction="row" spacing={2}>
+      <Navbar />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
         <Sidebar />
         <Feed />
         <Rightbar />
--- responsive-mui/src/components/Navbar.jsx
+++ responsive-mui/src/components/Navbar.jsx
@@ -1,8 +1,61 @@
-import { Box } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Toolbar,
+  Typography,
+  styled,
+} from "@mui/material";
+import LunchDiningIcon from "@mui/icons-material/LunchDining";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 import React from "react";
 
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: "8px",
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "20px",
+}));
+
 const Navbar = () => {
-  return <Box bgcolor="lightyellow">Navbar</Box>;
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <LunchDiningIcon />
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Navbar
+        </Typography>
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <ChatBubbleIcon />
+          </Badge>
+          <Badge badgeContent={134} color="error">
+            <NotificationsIcon />
+          </Badge>
+          <Avatar
+            src="https://afarber.de/images/farber.jpg"
+            sx={{ width: 30, height: 30 }}
+          ></Avatar>
+        </Icons>
+      </StyledToolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
