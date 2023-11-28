8886bea735439a38cfae49758a5f15157fa0d81d
Alexander Farber <farber72@outlook.de>
2023-11-28 16:43:22 +0100

    Display 3 buttons with icons

--- responsive-mui/src/App.js
+++ responsive-mui/src/App.js
@@ -1,19 +1,18 @@
+import { Button } from "@mui/material";
+import SettingsIcon from "@mui/icons-material/Settings";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button variant="text" startIcon={<SettingsIcon />}>
+        Text
+      </Button>
+      <Button variant="contained" startIcon={<SettingsIcon />}>
+        Contained
+      </Button>
+      <Button variant="outlined" startIcon={<SettingsIcon />}>
+        Outlined
+      </Button>
     </div>
   );
 }
