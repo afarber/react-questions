007d4dc6c36c44cca8c1d8e279bad5a92899ba02
Alexander Farber <farber72@outlook.de>
2023-11-29 20:12:35 +0100

    Add a list with 4 items to the side bar

--- responsive-mui/src/components/Sidebar.jsx
+++ responsive-mui/src/components/Sidebar.jsx
@@ -1,4 +1,13 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
 import React from "react";
 
 const Sidebar = () => {
@@ -9,7 +18,34 @@ const Sidebar = () => {
       p={2}
       sx={{ display: { xs: "none", sm: "block" } }}
     >
-      Sidebar
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="Inbox" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <DraftsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Drafts" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemText primary="Trash" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemText primary="Spam" />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
