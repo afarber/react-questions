65c8b972fa192208f263d428ff14afacd6584a1a
Alexander Farber <farber72@outlook.de>
2023-12-12 10:44:11 +0100

    Add a collapsible @mui/material/Table from the doc example

--- top-list/src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="App">TODO display table here</div>;
-}
-
-export default App;
---
+++ top-list/src/App.jsx
@@ -0,0 +1,6 @@
+import "./App.css";
+import TopTable from "./components/TopTable";
+
+export default function App() {
+  return <><h1>Top List</h1><TopTable /></>;
+}
