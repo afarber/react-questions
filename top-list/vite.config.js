04bb38d5ba97c9a4d6f2789bc2135ea6b0faebea
Alexander Farber <farber72@outlook.de>
2024-01-16 15:21:45 +0100

    Do not use "assets" sub folder for the generated files

--- top-list/vite.config.js
+++ top-list/vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     target: "es2015",
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
       },
     },
   },
