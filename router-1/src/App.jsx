1f62c01fc946c211fae5fa68e6a101071b3c1b12
Alexander Farber <farber72@outlook.de>
2024-01-28 18:00:14 +0100

    Add a 404 page

--- router-1/src/App.jsx
+++ router-1/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import PageOne from "./pages/PageOne";
 import PageTwo from "./pages/PageTwo";
 import PageThree from "./pages/PageThree";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
@@ -11,18 +12,19 @@ function App() {
       <BrowserRouter>
         <nav>
           <Link to="/">Home</Link>
-          {" | "}
+          &nbsp;|&nbsp;
           <Link to="/PageOne">PageOne</Link>
-          {" | "}
+          &nbsp;|&nbsp;
           <Link to="/PageTwo">PageTwo</Link>
-          {" | "}
+          &nbsp;|&nbsp;
           <Link to="/PageThree">PageThree</Link>
         </nav>
         <Routes>
           <Route path="/" element={<PageOne />} />
-          <Route path="/PageOne" element={<PageOne />} />
-          <Route path="/PageTwo" element={<PageTwo />} />
-          <Route path="/PageThree" element={<PageThree />} />
+          <Route path="PageOne" element={<PageOne />} />
+          <Route path="PageTwo" element={<PageTwo />} />
+          <Route path="PageThree" element={<PageThree />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
     </>
