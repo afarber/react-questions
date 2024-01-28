efe80bf505b2f989c92970f13ad376685b3b0fb9
Alexander Farber <farber72@outlook.de>
2024-01-28 23:02:23 +0100

    Add app bar

--- router-1/src/App.jsx
+++ router-1/src/App.jsx
@@ -1,32 +1,49 @@
 import "./App.css";
-import LeftDrawer from "./components/LeftDrawer";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import PageOne from "./pages/PageOne";
 import PageTwo from "./pages/PageTwo";
 import PageThree from "./pages/PageThree";
 import PageNotFound from "./pages/PageNotFound";
+import { AppBar, Box, IconButton, Toolbar } from "@mui/material";
+import { Menu } from "@mui/icons-material";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <nav>
-          <Link to="/">Home</Link>
-          &nbsp;|&nbsp;
-          <Link to="/PageOne">PageOne</Link>
-          &nbsp;|&nbsp;
-          <Link to="/PageTwo">PageTwo</Link>
-          &nbsp;|&nbsp;
-          <Link to="/PageThree">PageThree</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<PageOne />} />
-          <Route path="PageOne" element={<PageOne />} />
-          <Route path="PageTwo" element={<PageTwo />} />
-          <Route path="PageThree" element={<PageThree />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Box>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              sx={{ mr: 2 }}
+            >
+              <Menu />
+            </IconButton>
+            Test
+          </Toolbar>
+        </AppBar>
+        {/* TODO add margin-top */}
+        <BrowserRouter>
+          <nav>
+            <Link to="/">Home</Link>
+            &nbsp;|&nbsp;
+            <Link to="/PageOne">PageOne</Link>
+            &nbsp;|&nbsp;
+            <Link to="/PageTwo">PageTwo</Link>
+            &nbsp;|&nbsp;
+            <Link to="/PageThree">PageThree</Link>
+          </nav>
+          <Routes>
+            <Route path="/" element={<PageOne />} />
+            <Route path="PageOne" element={<PageOne />} />
+            <Route path="PageTwo" element={<PageTwo />} />
+            <Route path="PageThree" element={<PageThree />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Box>
     </>
   );
 }
