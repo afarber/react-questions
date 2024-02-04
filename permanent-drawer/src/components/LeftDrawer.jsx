a360663cb5b6806b417d3956d5f3e4c3c53bbe5b
Alexander Farber <farber72@outlook.de>
2024-02-04 19:04:01 +0100

    Use the custom method MyListItem()

--- permanent-drawer/src/components/LeftDrawer.jsx
+++ permanent-drawer/src/components/LeftDrawer.jsx
@@ -78,14 +78,7 @@ export default function LeftDrawer() {
         <Divider />
         <List>
           {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
+            <MyListItem key={text} index={index} text={text} />
           ))}
         </List>
       </Drawer>
