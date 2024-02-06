db0fbc40984ebaf75b16321a0801b226d2a634cc
Alexander Farber <farber72@outlook.de>
2024-02-06 19:07:17 +0100

    Add Main page

--- permanent-drawer/src/App.jsx
+++ permanent-drawer/src/App.jsx
@@ -1,10 +1,29 @@
+import { AppBar, Box, CssBaseline, Toolbar, Typography } from "@mui/material";
 import "./App.css";
+import Main from "./pages/Main";
 import LeftDrawer from "./components/LeftDrawer";
 
 export default function App() {
   return (
     <>
-      <LeftDrawer />
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+          }}
+        >
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+              Permanent drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <LeftDrawer />
+        <Toolbar />
+        <Main />
+      </Box>
     </>
   );
 }
