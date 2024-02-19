678ec3bff1e55d02276340d0b3054e67b6b3c482
Alexander Farber <farber72@outlook.de>
2024-02-19 22:56:35 +0100

    Add 6 vite build commands

--- permanent-drawer/vite.config.js
+++ permanent-drawer/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const args = process.argv.slice(2);
+const lang =
+  args.find((arg) => arg.startsWith("--lang=")).split("=")[1] || "en";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
