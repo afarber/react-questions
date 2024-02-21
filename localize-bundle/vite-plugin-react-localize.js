af5462ace135f5495042cdba570dbf6f7e13a26f
Alexander Farber <farber72@outlook.de>
2024-02-21 23:33:11 +0100

    Use generateBundle hook

--- localize-bundle/vite-plugin-react-localize.js
+++ localize-bundle/vite-plugin-react-localize.js
@@ -1,5 +1,8 @@
 "use strict";
 
+import fs from "fs";
+import path from "path";
+
 const localizedStrings = {
   en: {
     __YES__: "Yes",
@@ -22,10 +25,29 @@ export default function localize(lang) {
   return {
     name: "localize-plugin",
     transform(code, id) {
-      console.log(lang, id);
+      //console.log(lang, id);
       return code.replaceAll(/__[A-Z]+__/g, function (match) {
         return localizedStrings[lang][match] || match;
       });
+    },
+    generateBundle(outputOptions, bundle) {
+      //console.log("outputOptions", outputOptions);
+      //console.log("bundle", bundle["index.js"].code.length);
+
+      // TODO iterate bundle dictionary instead of using find()
+      const indexJs = Object.keys(bundle).find((fileName) =>
+        fileName.endsWith("index.js")
+      );
+      if (indexJs) {
+        const sourcePath = path.resolve(outputOptions.dir, indexJs);
+        console.log("sourcePath", sourcePath);
+
+        for (const [lang, value] of Object.entries(localizedStrings)) {
+          const destPath = path.resolve(outputOptions.dir, `index-${lang}.js`);
+          console.log("destPath", destPath);
+          // TODO replace placeholders in bundle["index.js"].code
+        }
+      }
     },
   };
 }
