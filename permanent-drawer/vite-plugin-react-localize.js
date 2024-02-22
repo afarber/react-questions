4b3cdcaff272b8be2c481ee3c97eec7aea4b6dbf
Alexander Farber <farber72@outlook.de>
2024-02-22 12:09:55 +0100

    Generate the localized bundle files

--- permanent-drawer/vite-plugin-react-localize.js
+++ permanent-drawer/vite-plugin-react-localize.js
@@ -1,46 +1,85 @@
 "use strict";
 
+import fs from "fs";
+import path from "path";
+
 const localizedStrings = {
   en: {
+    __YES__: "Yes",
+    __NO__: "No",
+    __CANCEL__: "Cancel",
     __NEW_GAME__: "New game",
     __STATS__: "Statistics",
     __RATING__: "Rating",
   },
   de: {
+    __YES__: "Ja",
+    __NO__: "Nein",
+    __CANCEL__: "Abbrechen",
     __NEW_GAME__: "Neues Spiel",
     __STATS__: "Statistik",
     __RATING__: "Top",
   },
   fr: {
+    __YES__: "Oui",
+    __NO__: "Non",
+    __CANCEL__: "Annuler",
     __NEW_GAME__: "Nouveau jeu",
     __STATS__: "Statistics",
     __RATING__: "Top",
   },
   nl: {
+    __YES__: "Ja",
+    __NO__: "Nee",
+    __CANCEL__: "Annuleren",
     __NEW_GAME__: "Nieuw spel",
     __STATS__: "Statistics",
     __RATING__: "Top",
   },
   pl: {
+    __YES__: "Tak",
+    __NO__: "Nie",
+    __CANCEL__: "Anuluj",
     __NEW_GAME__: "Nowa gra",
     __STATS__: "Statistics",
     __RATING__: "Top",
   },
   ru: {
+    __YES__: "Да",
+    __NO__: "Нет",
+    __CANCEL__: "Отмена",
     __NEW_GAME__: "Новая игра",
     __STATS__: "Статистика",
     __RATING__: "Рейтинг",
   },
 };
 
-export default function localize(lang) {
+export default function localize() {
   return {
     name: "localize-plugin",
-    transform(code, id) {
-      console.log(lang, id);
-      return code.replaceAll(/__[_A-Z]+__/g, function (match) {
-        return localizedStrings[lang][match] || match;
-      });
+    generateBundle(outputOptions, bundle) {
+      for (const [fileName, bundleValue] of Object.entries(bundle)) {
+        if (!fileName.endsWith("index.js")) {
+          continue;
+        }
+        const indexJsPath = path.resolve(outputOptions.dir, fileName);
+        console.log("\nReplacing placeholders in", indexJsPath);
+
+        for (const lang of Object.keys(localizedStrings)) {
+          const indexLangPath = path.resolve(
+            outputOptions.dir,
+            `index-${lang}.js`
+          );
+          console.log("Creating localized file", indexLangPath);
+          const replacedContent = bundleValue.code.replaceAll(
+            /__[A-Z_]+?__/g,
+            function (match) {
+              return localizedStrings[lang][match] || match;
+            }
+          );
+          fs.writeFileSync(indexLangPath, replacedContent);
+        }
+      }
     },
   };
 }
--- permanent-drawer/vite.config.js
+++ permanent-drawer/vite.config.js
@@ -2,14 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import localize from "./vite-plugin-react-localize";
 
-const lastArg =
-  process.argv.length > 0 ? process.argv[process.argv.length - 1] : "";
-const matches = lastArg.match(/--lang=(en|de|fr|nl|pl|ru)$/);
-const lang = matches ? matches[1] : "en";
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), localize(lang)],
+  plugins: [react(), localize()],
   build: {
     target: "es2015",
     rollupOptions: {
