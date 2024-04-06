306f741c797a98e9063e46a39c6a87a4a96bfe10
Alexander Farber <farber72@outlook.de>
2024-04-06 23:10:16 +0200

    Add Drawer element (does not work yet)

--- bottom-drawer/src/App.jsx
+++ bottom-drawer/src/App.jsx
@@ -1,48 +1,30 @@
 "use client";
 
 import { useState } from "react";
-import TextField from "@mui/material/TextField";
-import { Box, InputAdornment } from "@mui/material";
+import Drawer from "@mui/material/Drawer";
 import { ThumbDown, ThumbUp } from "@mui/icons-material";
 
-const DICT = {
-  AA: "Rough, cindery lava",
-  AB: "An abdominal muscle",
-  ABS: "Abdominal muscles",
-  ABSQUATULATE: "",
-  ABSQUATULATED: "",
-  ABSQUATULATES: "",
-  ABSQUATULATING: "",
-  ABY: "Pay the penalty",
-};
+const ITEMS = ["Winter", "Spring", "Summer", "Autumn"];
 
 export default function App() {
-  const [found, setFound] = useState(false);
-  const [description, setDescription] = useState("");
+  const [state, setState] = useState(true);
 
-  const handleChange = (ev) => {
-    ev.preventDefault();
-    const key = ev.target.value.trim().toUpperCase();
-
-    // use the key and not word below, because the latter is updated async
-    if (key.length < 2) {
-      setFound(false);
-      setDescription("");
-    } else {
-      setFound(Object.prototype.hasOwnProperty.call(DICT, key));
-      setDescription(DICT[key] || "The word is not found in the dictionary");
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
     }
+
+    setState(open);
   };
 
   return (
-    <Box component="form" noValidate autoComplete="off">
-      <TextField id="wordInput" label="Enter a word" onChange={handleChange} />
-
-      <Box sx={{ p: 2 }}>
-        {found ? <ThumbUp color="primary" /> : <ThumbDown color="error" />}
-      </Box>
-
-      <Box>{description}</Box>
-    </Box>
+    <>
+      <Drawer anchor="bottom" open={state} onClose={toggleDrawer(false)}>
+        XXX {ITEMS}
+      </Drawer>
+    </>
   );
 }
