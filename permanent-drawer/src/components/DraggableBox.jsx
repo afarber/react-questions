fc4a2d0234ead2df68b044e83316dabac0f2b481
Alexander Farber <farber72@outlook.de>
2024-04-17 20:26:34 +0200

    Add DraggableBox and display 3 of them in the Main page

---
+++ permanent-drawer/src/components/DraggableBox.jsx
@@ -0,0 +1,64 @@
+import PropTypes from "prop-types";
+import { Texture } from "pixi.js-legacy";
+import { Sprite } from "@pixi/react";
+import React from "react";
+
+let index = 1;
+
+export const DraggableBox = ({ tint, x, y, ...props }) => {
+  const isDragging = React.useRef(false);
+  const offset = React.useRef({ x: 0, y: 0 });
+  const [position, setPosition] = React.useState({ x: x || 0, y: y || 0 });
+  const [alpha, setAlpha] = React.useState(1);
+  const [zIndex, setZIndex] = React.useState(index);
+
+  function onStart(e) {
+    isDragging.current = true;
+    offset.current = {
+      x: e.data.global.x - position.x,
+      y: e.data.global.y - position.y,
+    };
+
+    setAlpha(0.5);
+    setZIndex(index++);
+  }
+
+  function onEnd() {
+    isDragging.current = false;
+    setAlpha(1);
+  }
+
+  function onMove(e) {
+    if (isDragging.current) {
+      setPosition({
+        x: e.data.global.x - offset.current.x,
+        y: e.data.global.y - offset.current.y,
+      });
+    }
+  }
+
+  return (
+    <Sprite
+      {...props}
+      alpha={alpha}
+      position={position}
+      texture={Texture.WHITE}
+      width={100}
+      height={100}
+      zIndex={zIndex}
+      tint={tint}
+      eventMode="static"
+      pointerdown={onStart}
+      pointerup={onEnd}
+      pointerupoutside={onEnd}
+      pointermove={onMove}
+    />
+  );
+};
+
+DraggableBox.propTypes = {
+  tint: PropTypes.number.isRequired,
+  // the x, y arguments are optional
+  x: PropTypes.number,
+  y: PropTypes.number,
+};
--- permanent-drawer/src/pages/Main.jsx
+++ permanent-drawer/src/pages/Main.jsx
@@ -1,19 +1,20 @@
 "use client";
 
-import Box from "@mui/material/Box";
-import MainDrawer from "../components/MainDrawer";
-import { MyComponent } from "../components/MyComponent";
+import { Stage, Container } from "@pixi/react";
+import { DraggableBox } from "../components/DraggableBox";
+
+const width = 800;
+const height = 500;
+const backgroundColor = 0x8d9dad;
 
 export default function Main() {
   return (
-    <>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: "background.default", p: 1 }}
-      >
-        <MyComponent />
-      </Box>
-      <MainDrawer />
-    </>
+    <Stage width={width} height={height} options={{ backgroundColor }}>
+      <Container sortableChildren={true}>
+        <DraggableBox tint={0xff00ff} x={20} y={20} />
+        <DraggableBox tint={0x00ffff} x={140} y={20} />
+        <DraggableBox tint={0x00ff00} x={280} y={20} />
+      </Container>
+    </Stage>
   );
 }
