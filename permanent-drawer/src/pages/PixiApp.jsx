827435da2087148a31b6f794f6462f11ab1d9ffd
Alexander Farber <farber72@outlook.de>
2024-04-21 22:58:02 +0200

    Add resize event listener and remove padding from root div

--- permanent-drawer/src/pages/PixiApp.jsx
+++ permanent-drawer/src/pages/PixiApp.jsx
@@ -1,20 +1,34 @@
 "use client";
 
-import { Application, Point, Sprite, Texture } from "pixi.js-legacy";
+import { Application, Graphics, Point, Sprite, Texture } from "pixi.js-legacy";
 //import { DraggableBox } from "../components/DraggableBox";
 import { useEffect, useRef } from "react";
 
 const app = new Application({
-  width: 800,
-  height: 600,
   backgroundColor: 0x5bba6f,
+  autoResize: true,
 });
 
-// the relative offset point of the click on the tile
-let grabPoint = new Point();
-let draggedTile;
+const rectBlue = new Graphics().beginFill(0x0000ff).drawRect(0, 0, 200, 100);
+const rectRed = new Graphics()
+  .beginFill(0xff0000)
+  .drawRect(-200, -100, 200, 100);
+
+function resize() {
+  // Resize the renderer
+  app.renderer.resize(window.innerWidth - 260, window.innerHeight);
+  // You can use the 'screen' property as the renderer visible
+  // area, this is more useful than view.width/height because
+  // it handles resolution
+  rectBlue.position.set(0, 0);
+  rectRed.position.set(app.screen.width, app.screen.height);
+}
 
 export default function PixiApp() {
+  // the relative offset point of the click on the tile
+  let grabPoint = new Point();
+  let draggedTile;
+
   function onDragStart(event) {
     draggedTile = event.target;
     draggedTile.toLocal(event.global, null, grabPoint);
@@ -51,8 +65,12 @@ export default function PixiApp() {
   useEffect(() => {
     // On first render add app to DOM
     ref.current.appendChild(app.view);
+
     // Start the PixiJS app
     app.start();
+
+    app.stage.addChild(rectBlue);
+    app.stage.addChild(rectRed);
 
     const texture = Texture.from("https://pixijs.com/assets/bunny.png");
     const bunny = new Sprite(texture);
@@ -61,8 +79,11 @@ export default function PixiApp() {
     bunny.y = 70;
     bunny.width = 100;
     bunny.height = 100;
-
     app.stage.addChild(bunny);
+
+    // https://jsfiddle.net/bigtimebuddy/oaLwp0p9/
+    window.addEventListener("resize", resize);
+    resize();
 
     return () => {
       // On unload stop the application
@@ -70,5 +91,5 @@ export default function PixiApp() {
     };
   }, []);
 
-  return <div ref={ref} />;
+  return <div ref={ref} style={{ background: "#f0f" }} />;
 }
