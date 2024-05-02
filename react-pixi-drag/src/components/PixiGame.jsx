953f93a856c74b512209fe08846ce0a99baca351
Alexander Farber <farber72@outlook.de>
2024-05-02 11:26:29 +0200

    Add a separate Tile class file

--- react-pixi-drag/src/components/PixiGame.jsx
+++ react-pixi-drag/src/components/PixiGame.jsx
@@ -2,8 +2,9 @@
 
 import { useEffect, useRef } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Application, Graphics, Sprite, Texture } from "pixi.js-legacy";
+import { Application, Graphics, Point } from "pixi.js-legacy";
 import { useMediaQuery } from "@react-hook/media-query";
+import { CELL, Tile } from "../game/Tile";
 
 const PixiGame = () => {
   const isSmallScreen = useMediaQuery("(max-width: 640px)");
@@ -21,8 +22,8 @@ const PixiGame = () => {
 
     const app = new Application({
       backgroundColor: 0xccffcc,
-      width: 800,
-      height: 800,
+      width: 8 * CELL,
+      height: 8 * CELL,
       //view: childElement,
     });
 
@@ -50,26 +51,66 @@ const PixiGame = () => {
 
     resizeObserver.observe(parentElement);
 
+    // the relative offset point of the click on the tile
+    let grabPoint = new Point();
+    let draggedTile;
+
+    // The stage will handle the move events
+    app.stage.eventMode = "static";
+    app.stage.hitArea = app.screen;
+
     const background = new Graphics();
     for (let i = 0; i < 8; i++) {
       for (let j = 0; j < 8; j++) {
         if ((i + j) % 2 == 0) {
           background.beginFill(0xccccff);
-          background.drawRect(i * 100, j * 100, 100, 100);
+          background.drawRect(i * CELL, j * CELL, CELL, CELL);
           background.endFill();
         }
       }
     }
     app.stage.addChild(background);
 
-    const texture = Texture.from("https://pixijs.com/assets/bunny.png");
-    const bunny = new Sprite(texture);
-    bunny.anchor.set(0.5);
-    bunny.x = 50;
-    bunny.y = 50;
-    bunny.width = 100;
-    bunny.height = 100;
-    app.stage.addChild(bunny);
+    const r = new Tile("red", onDragStart, onDragEnd, 3, 3);
+    const g = new Tile("green", onDragStart, onDragEnd, 4, 3);
+    const b = new Tile("blue", onDragStart, onDragEnd, 5, 3);
+
+    app.stage.addChild(r);
+    app.stage.addChild(g);
+    app.stage.addChild(b);
+
+    function onDragStart({ target, global }) {
+      draggedTile = target;
+      draggedTile.toLocal(global, null, grabPoint);
+      grabPoint.x *= draggedTile.scale.x;
+      grabPoint.y *= draggedTile.scale.y;
+      app.stage.cursor = "pointer";
+      app.stage.on("pointermove", onDragMove);
+      // put the dragged object on the top
+      app.stage.removeChild(draggedTile);
+      app.stage.addChild(draggedTile);
+      draggedTile.startDragging();
+      console.log("onDragStart:", draggedTile.x, draggedTile.y);
+    }
+
+    function onDragMove({ global: { x, y } }) {
+      draggedTile.x = x - grabPoint.x;
+      draggedTile.y = y - grabPoint.y;
+      console.log("onDragMove:", draggedTile.x, draggedTile.y);
+    }
+
+    function onDragEnd() {
+      // reset the tile scale and calculate its col and row
+      draggedTile.stopDragging();
+      // the next 2 lines are not needed here, but
+      // in my real game the tile is put beneath the HUD
+      app.stage.removeChild(draggedTile);
+      app.stage.addChildAt(draggedTile, app.stage.children.length);
+      app.stage.cursor = null;
+      app.stage.off("pointermove", onDragMove);
+      console.log("onDragEnd:", draggedTile.x, draggedTile.y);
+      draggedTile = null;
+    }
 
     return () => {
       console.log("PixiGame unmount");
