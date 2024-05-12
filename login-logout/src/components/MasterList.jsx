70affe6cfb5e62dada250380cf9448cf857f0a2e
Alexander Farber <farber72@outlook.de>
2024-05-12 14:38:18 +0200

    Use prevState

--- login-logout/src/components/MasterList.jsx
+++ login-logout/src/components/MasterList.jsx
@@ -6,21 +6,15 @@ const MasterList = () => {
   const { games, setGames } = useContext(MyContext);
 
   const addNewGame = () => {
-    const newGame = {
-      id: Math.floor(Math.random() * 1000), // Generate a random integer id
-      title: `Game ${games.length + 1}`, // Set a default title
-    };
-    setGames([...games, newGame]);
+    setGames((prevState) => {
+      const id = Math.floor(Math.random() * 1000);
+      const newGame = {
+        id: id,
+        title: `Game ${id}`,
+      };
+      return [...prevState, newGame];
+    });
   };
-
-  /*
-  const games = [
-    { id: 1, title: "Game 1" },
-    { id: 2, title: "Game 2" },
-    { id: 3, title: "Game 3" },
-    { id: 4, title: "Game 4" },
-  ];
-*/
 
   return (
     <div className="left">
