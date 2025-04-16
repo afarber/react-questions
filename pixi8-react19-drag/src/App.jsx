bce9fb962adea05b31edff1f551900b4f3eec9a5
Alexander Farber <farber72@outlook.de>
2025-04-16 17:44:09 +0200

    Try to instantiate 3 tiles (does not work yet)

--- pixi8-react19-drag/src/App.jsx
+++ pixi8-react19-drag/src/App.jsx
@@ -1,12 +1,13 @@
 import {
   Application,
   extend,
+  useApplication
 } from '@pixi/react'
 import {
   Container,
   Graphics,
 } from 'pixi.js'
-import { useCallback } from 'react'
+import { useEffect, useCallback } from 'react'
 
 extend({
   Container,
@@ -20,6 +21,35 @@ const App = () => {
     graphics.rect(0, 0, 100, 100)
     graphics.fill()
   }, [])
+
+  const { app } = useApplication();
+
+  useEffect(() => {
+    if (app) {
+      app.stage.eventMode = "static";
+      app.stage.hitArea = app.screen;
+
+      const background = new Graphics();
+      for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+          if ((i + j) % 2 === 0) {
+            background.beginFill(0xccccff);
+            background.drawRect(i * CELL, j * CELL, CELL, CELL);
+            background.endFill();
+          }
+        }
+      }
+      app.stage.addChild(background);
+
+      const r = new Tile("red", onDragStart, onDragEnd, 3, 3);
+      const g = new Tile("green", onDragStart, onDragEnd, 4, 3);
+      const b = new Tile("blue", onDragStart, onDragEnd, 5, 3);
+
+      app.stage.addChild(r);
+      app.stage.addChild(g);
+      app.stage.addChild(b);
+    }
+  }, []);
 
   return (
     <Application resizeTo={window} backgroundColor={0xCCFFCC}>
