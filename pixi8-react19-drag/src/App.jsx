fd78a9e4f1369f32be7a635e969ce30b6839bd50
Alexander Farber <farber72@outlook.de>
2025-04-18 12:25:22 +0200

    Add custom component Game.jsx

--- pixi8-react19-drag/src/App.jsx
+++ pixi8-react19-drag/src/App.jsx
@@ -1,73 +1,18 @@
-import {
-  Application,
-  extend,
-  useApplication
-} from '@pixi/react'
-import {
-  Container,
-  Graphics,
-} from 'pixi.js'
-import { useEffect, useCallback } from 'react'
+import { Application, extend } from "@pixi/react";
+import { Container, Graphics } from "pixi.js";
+import Game from "./Game";
 
 extend({
   Container,
   Graphics,
-})
-
-const CELL = 100;
+});
 
 const App = () => {
-  const drawCallback = useCallback(graphics => {
-    graphics.clear()
-    graphics.setFillStyle({ color: '0xCCCCFF' })
-
-    for (let i = 0; i < 8; i++) {
-      for (let j = 0; j < 8; j++) {
-        if ((i + j) % 2 === 0) {
-          graphics.rect(i * CELL, j * CELL, CELL, CELL);
-          graphics.fill();
-        }
-      }
-    }
-  }, [])
-
-  
-  /*
-  useEffect(() => {
-    if (app) {
-      app.stage.eventMode = "static";
-      app.stage.hitArea = app.screen;
-
-      const background = new Graphics();
-      for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-          if ((i + j) % 2 === 0) {
-            background.beginFill(0xccccff);
-            background.drawRect(i * CELL, j * CELL, CELL, CELL);
-            background.endFill();
-          }
-        }
-      }
-      app.stage.addChild(background);
-
-      const r = new Tile("red", onDragStart, onDragEnd, 3, 3);
-      const g = new Tile("green", onDragStart, onDragEnd, 4, 3);
-      const b = new Tile("blue", onDragStart, onDragEnd, 5, 3);
-
-      app.stage.addChild(r);
-      app.stage.addChild(g);
-      app.stage.addChild(b);
-    }
-  }, []);
-  */
-
   return (
-    <Application resizeTo={window} backgroundColor={0xCCFFCC}>
-      <pixiContainer x={0} y={0}>
-        <pixiGraphics draw={drawCallback} />
-      </pixiContainer>
+    <Application resizeTo={window} backgroundColor={0xccffcc}>
+      <Game />
     </Application>
-  )
-}
+  );
+};
 
-export default App
+export default App;
