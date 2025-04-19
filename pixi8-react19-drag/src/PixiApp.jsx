04a0027319bee51f91a2cd44c99bdf4a3636e369
Alexander Farber <farber72@outlook.de>
2025-04-19 09:59:54 +0200

    Add vertical flex

--- pixi8-react19-drag/src/PixiApp.jsx
+++ pixi8-react19-drag/src/PixiApp.jsx
@@ -1,11 +1,34 @@
+import { useRef } from "react";
 import { Application } from "@pixi/react";
 import PixiGame from "./PixiGame";
 
 const PixiApp = () => {
+  const parentRef = useRef(null);
+
   return (
-    <Application resizeTo={window} backgroundColor={0xccffcc}>
-      <PixiGame />
-    </Application>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-around",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div style={{ background: "lightblue" }}>Game Score</div>
+      <div
+        ref={parentRef}
+        style={{
+          background: "lightgreen",
+          flexGrow: 1,
+        }}
+      >
+        <Application resizeTo={parentRef} backgroundColor={0xccffcc}>
+          <PixiGame />
+        </Application>
+      </div>
+      <div style={{ background: "lightpink" }}>Game Hint</div>
+    </div>
   );
 };
 
